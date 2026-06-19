273d5742105bbec0fb924d154820a21585e50993
Maria <m76131543@gmail.com>
2026-06-19 21:23:04 +0530

    Implement boolean check for integer comparison
    Add boolean check for two integers input comparison.

---
+++ first_module.py/boolean_check.py
@@ -0,0 +1,5 @@
+# WAP to input two integer numbers a and b. Print True if a greater than equal  to b, if not print false.
+
+a=int(input("Enter first no:"))
+b=int(input("Enter second no:"))
+print(a>=b)
