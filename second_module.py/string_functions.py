d29a7079e6f5c6f6a3375b5a2c21c0e16f19f3f4
Maria <m76131543@gmail.com>
2026-06-21 13:08:18 +0530

    Create string_functions.py

---
+++ second_module.py/string_functions.py
@@ -0,0 +1,11 @@
+str = " i am learning Python."
+
+print (str.endswith("thon."))               #return true or false if string ends with substr.
+
+print(str.replace("Python","javascript"))      # replace all occurences of old
+
+
+print(str.find("i"))     # index of first occurence.
+
+
+print(str.count("i"))      # coount the occurence of substr.
