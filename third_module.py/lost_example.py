114f98ea3112a24b64445c78065e9e6267b0b71f
Maria <m76131543@gmail.com>
2026-06-22 21:09:47 +0530

    Add script to input favorite movies into a list
    Implemented a script to collect and display user's favorite movies.

---
+++ third_module.py/lost_example.py
@@ -0,0 +1,12 @@
+# 1. WAP to ask the user to enter the names of their 3 favourite movies and store them in a list.
+
+movies = []
+m1 = input("Enter 1st movie:")
+m2 = input("Enter 2nd movie:")
+m3 = input("Enter 3rd movie:")
+
+movies.append(m1)
+movies.append(m2)
+movies.append(m3)
+
+print(movies)
