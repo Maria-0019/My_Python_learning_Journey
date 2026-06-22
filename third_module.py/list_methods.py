7746223696f302f97a7701ef5eb8687256fb12ad
Maria <m76131543@gmail.com>
2026-06-22 21:05:45 +0530

    Create list_methods.py

---
+++ third_module.py/list_methods.py
@@ -0,0 +1,18 @@
+list = [2,5,6,3,1]
+list.append(8)              #adds one element at the end.
+print(list)
+
+list.sort()
+print(list)                 #sort in ascending order.
+
+list.sort(reverse=True)     # sort in descending order.
+print(list)
+
+list.insert(3,4)            # insert element at index. 
+print(list)           
+
+list.remove(3)              #remove first of element.
+print(list)
+
+list.pop(2)               # removes element at index.
+print(list)
