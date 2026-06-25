c04b4559adb65a24edeeb47a5f37ac0793a49735
Maria <m76131543@gmail.com>
2026-06-25 11:26:28 +0530

    Create range.py

---
+++ fifth_module/range.py
@@ -0,0 +1,17 @@
+# Print numbers from 0 to 100
+
+for i in range(101):     # rane(stop) 
+    print(i)   
+
+# Print numbers from 100 to 0
+
+for i in range (100,-1,-1):  # range(start, stop, step)
+    print(i)
+
+
+# Print multiplication table of  a number n.
+
+n=3
+
+for i in range(1,11):
+    print(n*i)
