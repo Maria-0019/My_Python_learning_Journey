83040fba957cb19479e49e2ddfa1aee537118e64
Maria <m76131543@gmail.com>
2026-06-25 11:01:27 +0530

    Update search_num.py

--- fifth_module/search_num.py
+++ fifth_module/search_num.py
@@ -1,10 +1,11 @@
- #Search a number x in the tuple using loop
+ # Search a number x in the tuple using loop
 
 nums=(23, 34, 12, 78, 65, 21, 11, 90, 32)
 X=21
 i=0
 while i<(len(nums)): 
-    if nums [i] == X:
-        print("found at index:")
-        i+=1
+    if nums[i]==X:
+        print("found at index:",i)
+    i+=1
+    print("finding...")
 
