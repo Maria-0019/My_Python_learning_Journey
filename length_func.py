aa20b1958c4e5e4d6413a021f426cc81e40daf2c
Maria <m76131543@gmail.com>
2026-06-27 12:39:13 +0530

    Create length_func.py

---
+++ length_func.py
@@ -0,0 +1,11 @@
+# WAF to print the length of the list.
+
+fruits=["Apple", "Mango", "Banana", "Litchi", "Pineapple"]
+cities=["delhi", "pune", "noida", "mumbai"]
+
+def cal_len(list):
+    print(len(list))
+
+
+cal_len(fruits)
+cal_len(cities)
