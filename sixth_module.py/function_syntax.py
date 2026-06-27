52ce20856741a92b27dd7948ae7f82ebffe82d10
Maria <m76131543@gmail.com>
2026-06-27 12:47:12 +0530

    Create function_syntax.py

---
+++ sixth_module.py/function_syntax.py
@@ -0,0 +1,13 @@
+#sum of two numbers
+
+#Function definition
+def calc_sum(a,b):    #paramters
+    sum =a+b
+    print(sum )
+    return sum
+
+calc_sum(3,4)        # function call; arguments
+
+calc_sum(5,10)                       #arguments
+
+calc_sum(7,5)                        #arguments
