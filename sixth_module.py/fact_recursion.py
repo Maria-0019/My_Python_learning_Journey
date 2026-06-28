3fa962924de3a96e16828a1af76d058a8889f2c4
Maria <m76131543@gmail.com>
2026-06-28 13:22:30 +0530

    Create fact_recursion.py

---
+++ sixth_module.py/fact_recursion.py
@@ -0,0 +1,12 @@
+# WAF to find the factorial of n number.4
+
+n=int(input("enter a number:"))
+def fact(n):
+    if(n==0 or n==1):
+        return 1
+    else:
+          return  n*fact(n-1)
+        
+    
+
+print("factorial:",fact(n) )
