bb1d0a2577cb69b9000804955959b61ab6bd44fd
Maria <m76131543@gmail.com>
2026-07-08 23:49:16 +0530

    Add multiple inheritance example with Fruit and Color

---
+++ OOPs_module.py/multiple_inheritance.py
@@ -0,0 +1,14 @@
+# Parent Class 1
+class Fruit:
+    def taste(self):
+        print("Fruits are sweet ")
+
+# Parent class 2
+class Color:
+    def color(self):
+        print("Fruits have colors 🌈")
+
+# Child class (inherits both)
+class Apple(Fruit, Color):
+    def info(self):
+        print("Apple is healthy")
