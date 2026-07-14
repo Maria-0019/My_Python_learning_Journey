3b89f05983c988f7f2dfd1eb48993ed0c9b8f40f
Maria <m76131543@gmail.com>
2026-07-14 21:02:39 +0530

    Create reverse_number.py

---
+++ Practice_Programs.py/reverse_number.py
@@ -0,0 +1,10 @@
+# WAP that reverses the given number.
+N=int(input("Enter a number:"))
+reverse=0
+
+while(N>0):
+    digit=N%10
+    reverse= reverse*10 + digit
+    N=N//10
+
+print("Reversed Number:", reverse)
