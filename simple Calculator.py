c282dd9a4ea4c9fd446f68f0c5fa2c787b0bfad1
Maria <m76131543@gmail.com>
2026-08-01 12:28:41 +0530

    Implement simple calculator with basic operations

---
+++ simple Calculator.py
@@ -0,0 +1,18 @@
+a=int(input("Enter 1st number:")
+b=int(input("Enter 2nd number:")
+
+def add(a,b):
+  return a+b
+def subtract(a,b):
+  return a-b
+def multiply(a,b):
+  return a*b
+def divide(a,b):
+  if (b==0):
+    return "ERROR: cannot divide !"
+    return a/b
+
+print ("Simple Calculator")
+print("1.Add=", add(a,b))
+print("2.Subtract=", subtract(a,b))
+print("3.Divide=", divide(a,b))
